9f685cdfba8fa1da1c10fac576ee83ed4a20b34e
Peter Teuben <teuben@gmail.com>
2018-03-01 12:03:11 -0500

    cleaned up

--- workflows/example1.py
+++ workflows/example1.py
@@ -22,6 +22,8 @@ cmds = [
 ]
 
 
+# getting bootstrapped with the right data in your directory is a bit painful...
+# either use the cmds[] or manually symlink
 if False:
     for cmd in cmds:
         print "CMD:",cmd
@@ -38,10 +40,8 @@ else:
         print "ln -s data/M100_12m.ptg"
         sys.exit(1)
 
-#   execfile('tp2vis.py')           # Assume this was done and it's in the environment now
-
-
-#   the two split() steps take a lot of time and diskspace, so are protected to run again
+# the two split() calls below take a lot of time and diskspace, so are protected to run again if you have the smaller datasets
+# however, the two "smaller" datasets are at the native vel resolution. See workflow6 for even smalller (<200MB) data at 5 km/s!
 #   M100_Band3_12m_CalibratedData.ms     16.7 GB
 #   M100_Band3_7m_CalibratedData.ms       5.5 GB
 #   M100_12m_CO.ms                        4.2 GB
@@ -51,62 +51,72 @@ if not os.path.exists('M100_12m_CO.ms'):
 if not os.path.exists('M100_7m_CO.ms'):
     split(vis='M100_Band3_7m_CalibratedData.ms',outputvis='M100_7m_CO.ms',spw='3,5',field='M100',datacolumn='data',keepflags=False)
 
-# none of the code below deals very well with files that exist, so don't even think about re-running this.
 
-
+# this script does attempt to remove all files that need to be rewritten....
 os.system('rm -rf M100_07m12m_CO_dirty.* M100_07m12m_CO_clean.*')
 
-tclean(vis=['M100_12m_CO.ms','M100_7m_CO.ms'],imagename='M100_07m12m_CO_dirty', niter=0,    gridder='mosaic',imsize=800,cell='0.5arcsec',phasecenter='J2000 12h22m54.9 +15d49m15',weighting='natural',threshold='0mJy',specmode='cube',outframe='LSRK',restfreq='115.271201800GHz',nchan=70,start='1400km/s',width='5km/s')
-tclean(vis=['M100_12m_CO.ms','M100_7m_CO.ms'],imagename='M100_07m12m_CO_clean', niter=10000,gridder='mosaic',imsize=800,cell='0.5arcsec',phasecenter='J2000 12h22m54.9 +15d49m15',weighting='natural',threshold='0mJy',specmode='cube',outframe='LSRK',restfreq='115.271201800GHz',nchan=70,start='1400km/s',width='5km/s')
+tclean(vis=['M100_12m_CO.ms','M100_7m_CO.ms'],imagename='M100_07m12m_CO_dirty', niter=0,
+       gridder='mosaic',imsize=800,cell='0.5arcsec',phasecenter='J2000 12h22m54.9 +15d49m15',weighting='natural',
+       threshold='0mJy',specmode='cube',outframe='LSRK',restfreq='115.271201800GHz',nchan=70,start='1400km/s',width='5km/s')
+tclean(vis=['M100_12m_CO.ms','M100_7m_CO.ms'],imagename='M100_07m12m_CO_clean', niter=10000,
+       gridder='mosaic',imsize=800,cell='0.5arcsec',phasecenter='J2000 12h22m54.9 +15d49m15',weighting='natural',
+       threshold='0mJy',specmode='cube',outframe='LSRK',restfreq='115.271201800GHz',nchan=70,start='1400km/s',width='5km/s')
 
 #
 
-#imview(raster=[{'file': 'M100_07m12m_CO_dirty.image'}],zoom={'channel':23,'blc': [200,200], 'trc': [600,600]},out='M100_07m12m_CO_dirty.23.png')
-#imview(raster=[{'file': 'M100_07m12m_CO_dirty.image'}],zoom={'channel':47,'blc': [200,200], 'trc': [600,600]},out='M100_07m12m_CO_dirty.47.png')
+imview(raster=[{'file': 'M100_07m12m_CO_clean.image'}],
+       zoom={'channel':24,'blc': [200,200], 'trc': [600,600]},
+       out='M100_07m12m_CO_clean.24.png')
 
 
-rms =  imstat('M100_TP_CO_cube.bl.image',axes=[0,1])['rms'][:6].mean()
+rms =  imstat('M100_TP_CO_cube.bl.image',axes=[0,1])['rms'][:6].mean()     # should be 0.153....
 
 os.system('rm -rf M100_TP_CO.ms')
 tp2vis('M100_TP_CO_cube.bl.image','M100_TP_CO.ms','M100_12m.ptg',nvgrp=20,rms=0.15379972353470259)
 
+
 os.system('rm -rf M100_TP07m12m_CO.ms')
 concat(vis=['M100_TP_CO.ms','M100_7m_CO.ms','M100_12m_CO.ms'],concatvis='M100_TP07m12m_CO.ms',copypointing=False)
 
-
 os.system('rm -rf M100_TP07m12m_CO_dirty.* M100_TP07m12m_CO_clean.*')
-tclean(vis='M100_TP07m12m_CO.ms',imagename='M100_TP07m12m_CO_dirty', niter=0,    gridder='mosaic',imsize=800,cell='0.5arcsec',phasecenter='J2000 12h22m54.9 +15d49m15',weighting='natural',threshold='0mJy',specmode='cube',outframe='LSRK',restfreq='115.271201800GHz',nchan=70,start='1400km/s',width='5km/s')
-tclean(vis='M100_TP07m12m_CO.ms',imagename='M100_TP07m12m_CO_clean', niter=10000,gridder='mosaic',imsize=800,cell='0.5arcsec',phasecenter='J2000 12h22m54.9 +15d49m15',weighting='natural',threshold='0mJy',specmode='cube',outframe='LSRK',restfreq='115.271201800GHz',nchan=70,start='1400km/s',width='5km/s')
+tclean(vis='M100_TP07m12m_CO.ms',imagename='M100_TP07m12m_CO_dirty', niter=0,
+       gridder='mosaic',imsize=800,cell='0.5arcsec',phasecenter='J2000 12h22m54.9 +15d49m15',weighting='natural',
+       threshold='0mJy',specmode='cube',outframe='LSRK',restfreq='115.271201800GHz',nchan=70,start='1400km/s',width='5km/s')
+tclean(vis='M100_TP07m12m_CO.ms',imagename='M100_TP07m12m_CO_clean', niter=10000,
+       gridder='mosaic',imsize=800,cell='0.5arcsec',phasecenter='J2000 12h22m54.9 +15d49m15',weighting='natural',
+       threshold='0mJy',specmode='cube',outframe='LSRK',restfreq='115.271201800GHz',nchan=70,start='1400km/s',width='5km/s')
 
 os.system('rm -rf difference.im')
 immath(imagename=['M100_TP07m12m_CO_dirty.image','M100_07m12m_CO_dirty.image'],expr='IM0-IM1',outfile='difference.im')
 
 
-#imview(raster=[{'file': 'M100_TP07m12m_CO_dirty.image'}],zoom={'channel':23,'blc': [200,200], 'trc': [600,600]},out='M100_TP07m12m_CO_dirty.23.png')
-#imview(raster=[{'file': 'M100_TP07m12m_CO_dirty.image'}],zoom={'channel':47,'blc': [200,200], 'trc': [600,600]},out='M100_TP07m12m_CO_dirty.47.png')
+imview(raster=[{'file': 'M100_TP07m12m_CO_clean.image'}],
+       zoom={'channel':24,'blc': [200,200], 'trc': [600,600]},
+       out='M100_TP07m12m_CO_dirty.24.png')
 
 tp2viswt('M100_TP_CO.ms',mode='stat')
-
 tp2viswt(['M100_7m_CO.ms','M100_12m_CO.ms'],mode='stat')
 
 
 tp2vispl(['M100_TP_CO.ms','M100_7m_CO.ms','M100_12m_CO.ms'],outfig="plot_tp2viswt_rms.png")
 
 tp2viswt("M100_TP_CO.ms",mode='const',value=0.015)
-
 tp2viswt("M100_TP_CO.ms",mode='multiply',value=10.0)
-
 tp2viswt("M100_TP_CO.ms",mode='rms',value=0.15379972353470259)
-
-
 tp2viswt(['M100_TP_CO.ms','M100_7m_CO.ms','M100_12m_CO.ms'],mode='beam',makepsf=True)      # this takes time
+
+tp2vispl(['M100_TP_CO.ms','M100_7m_CO.ms','M100_12m_CO.ms'],outfig="plot_tp2viswt_beam.png")
 
 os.system('rm -rf M100_TP07m12m_CO.ms')
 concat(vis=['M100_TP_CO.ms','M100_7m_CO.ms','M100_12m_CO.ms'],concatvis='M100_TP07m12m_CO.ms',copypointing=False)
 
 os.system('rm -rf M100_TP07m12m_CO_dirty.*  M100_TP07m12m_CO_clean.*')
-tclean(vis='M100_TP07m12m_CO.ms',imagename='M100_TP07m12m_CO_dirty',niter=0,    gridder='mosaic',imsize=800,cell='0.5arcsec',phasecenter='J2000 12h22m54.9 +15d49m15',weighting='natural',threshold='0mJy',specmode='cube',outframe='LSRK',restfreq='115.271201800GHz',nchan=70,start='1400km/s',width='5km/s')
-tclean(vis='M100_TP07m12m_CO.ms',imagename='M100_TP07m12m_CO_clean',niter=10000,gridder='mosaic',imsize=800,cell='0.5arcsec',phasecenter='J2000 12h22m54.9 +15d49m15',weighting='natural',threshold='0mJy',specmode='cube',outframe='LSRK',restfreq='115.271201800GHz',nchan=70,start='1400km/s',width='5km/s')
+tclean(vis='M100_TP07m12m_CO.ms',imagename='M100_TP07m12m_CO_dirty',niter=0,
+       gridder='mosaic',imsize=800,cell='0.5arcsec',phasecenter='J2000 12h22m54.9 +15d49m15',weighting='natural',
+       threshold='0mJy',specmode='cube',outframe='LSRK',restfreq='115.271201800GHz',nchan=70,start='1400km/s',width='5km/s')
+tclean(vis='M100_TP07m12m_CO.ms',imagename='M100_TP07m12m_CO_clean',niter=10000,
+       gridder='mosaic',imsize=800,cell='0.5arcsec',phasecenter='J2000 12h22m54.9 +15d49m15',weighting='natural',
+       threshold='0mJy',specmode='cube',outframe='LSRK',restfreq='115.271201800GHz',nchan=70,start='1400km/s',width='5km/s')
 
 
 # outside scope, but useful
