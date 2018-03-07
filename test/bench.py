e4876391e9b0dd5e7fd5f1ce5cce1176c37b35fc
Peter Teuben <teuben@gmail.com>
2018-03-07 13:51:34 -0500

    few more data, add MacOSX info

--- test/bench.py
+++ test/bench.py
@@ -1,31 +1,50 @@
 #  -*- python -*-
 #
-#  QAC benchmark, based on M100 workflow6 with just 5 channels
+#  QAC benchmark for TP2VIS:   based on M100 workflow6 with just 5 channels
+#
 #  Grab data from:     http://admit.astro.umd.edu/~teuben/QAC/qac_bench.tar.gz
 #  Benchmark run:      time casa --nogui -c bench.py
-#  Space needs:        ~500MB 
+#  Space needs:        ~500MB
+#  Memory needed:      ~2.6GB
 #
-#  Example run:        casa --nogui -c bench.py niter='[0,100,300,1000,3000,10000]'
+#  Example run:        casa --nogui -c bench.py niter='[0,100,300,1000,3000,10000]'  nvgrp=16
+#
+#  On a Mac you may need to use  /Applications/CASA.app/Contents/MacOSX/casa
 #
 #  Paper shows channels 1465,1485,1505 km/s
 #  Online example1 one shows channel 1520 km/s
-#
-
 #
 #           clean=0     tweak=0        full bench
 #  x270:   38" (29")  2'54" (7'43")   2"57" (7'48")
 #          51" (35")  2'56" (7'39")   3"04" (7'47")
 #
+#  Full bench: (some variations on the same machine I don't understand yet)
 #
-#  x270:    740.188u 19.676s 5:35.37 226.5%	0+0k 667504+3014296io 1124pf+0w      i7-3630QM CPU @ 2.40GHz                                              
+#  x270:    740.188u 19.676s 5:35.37 226.5%	0+0k 667504+3014296io 1124pf+0w      i7-3630QM CPU @ 2.40GHz
+#
 #  t530:    738.632u 17.956s 5:05.12 247.9%	0+0k 96792+2968744io 578pf+0w        i7-3630QM CPU @ 2.40GHz
 #           748.144u 19.440s 5:02.15 254.0%	0+0k 2104+2799288io 2pf+0w
+#
 #  dante:   385.062u 14.358s 2:42.37 245.9%	0+0k 624+2013376io 0pf+0w            i7-3820 CPU @ 3.60GHz
-#  sdp:     1743.867u 66.479s 4:53.99 615.7%	0+0k 409992+2944384io 242pf+0w       X5550  @ 2.67GHz
+#           393.835u 17.946s 2:42.70 253.0%	0+0k 218248+2016744io 240pf+0w (HDD)
+#           402.181u 14.695s 2:15.24 308.2%	0+0k 616+104io 0pf+0w (SHMEM)
+#
+#  sdp:     1743.867u 66.479s 4:53.99 615.7%	0+0k 409992+2944384io 242pf+0w       X5550  @ 2.67GH
+#            871.961u 56.835s 3:21.61 460.6%	0+0k 146672+2533464io 54pf+0w
+#            873.329u 58.793s 3:25.65 453.2%	0+0k 151560+2145232io 198pf+0w
+#
 #  chara:   1326.660u 24.061s 10:21.12 217.4%	0+0k 521080+104io 304pf+0w           i7 CPU  870  @ 2.93GHz  in /dev/shmem
 #            673.496u 17.252s 3:46.83 304.5%	0+0k 712+120io 2pf+0w
 #            603.262u 17.853s 3:30.83 294.6%	0+0k 768+128io 4pf+0w
 #            688.524u 20.112s 3:51.38 306.2%	0+0k 608+128io 0pf+0w
+#            678.992u 19.364s 3:44.14 311.5%	0+0k 323240+136io 323pf+0w (SHMEM)
+#            910.227u 20.115s 6:22.68 243.1%	0+0k 912+3244072io 0pf+0w  (HDD)
+#
+#  cvspost:  709.250u 52.919s 2:51.36 444.7% 0+0k 565664+4425384io 233pf+0w   (HDD)  E5-2640 v3 @ 2.60GHz
+#            691.213u 51.565s 2:22.13 522.5% 0+0k 48+80io 0pf+0w
+#
+
+
 
 # parameters in this benchmark workflow
 test        = 'bench' 
