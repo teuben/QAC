a599a0afa7df1dda1654609a166f02457c81d818
Peter Teuben <teuben@gmail.com>
2018-03-08 09:28:54 -0500

    versioning this file

--- casa.init.py
+++ casa.init.py
@@ -1,15 +1,19 @@
 #
 #  this is a piece of code you can place in ~/.casa/init.py
-#  to automatically start up QAC in your CASA session 
+#  to automatically start up QAC in your CASA session
+#  Check future versions of the QAC/casa.init.py code if
+#  their procedure has not changed
+#  The first time you can do:
+#       cat casa.init.py >> ~/.casa/init.py
 #
 
 import os, sys
 
-  
+#   QAC version 8-mar-2018  
 try:
     if sys.path[0] != "":   sys.path.insert(0,'')                  # CASA takes out the '' . why ???
     qac_root  = os.environ['HOME'] + '/.casa/QAC'                  # SET THIS TO YOUR LOCATION OF QAC or use a symlink
-    py_files  = ['src/qac', 'distribute/tp2vis', 'tp2vis/tp2vis']  # pick which ones you want
+    py_files  = ['src/qac', 'src/ssc', 'distribute/tp2vis', 'tp2vis/tp2vis']  
     work_dir = os.getcwd()
     os.chdir(qac_root)
     print "QAC: Root ",qac_root
