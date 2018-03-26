550d1679973356f230ef5641d45b28d375961ce7
Peter Teuben <teuben@gmail.com>
2018-03-26 11:30:29 -0400

    still work in progress

--- test/sky1.py
+++ test/sky1.py
@@ -1,15 +1,15 @@
-#
+# -*- python -*-
 #
 #  Play with the skymodel for SD2018
 #     - one or full pointing set
-#     - JD/tp2vis , feather, ssc
+#     - options for tp2vis, feather, ssc
 #
-#  Reminders:
-#  The 12m PB is 50" (FWHM)   [FWHM" ~ 600/DishDiam]
+#  Reminder: at 115 GHz we have:
+#      12m PB is 50" (FWHM)   [FWHM" ~ 600/DishDiam]
 #       7m PB is 85"
 #
 #  Timing:
-#           38'++
+#           30' on T530
 #  Memory:
 #           1.7GB +  3.0
 #  Space:
@@ -28,20 +28,32 @@ pixel_m      = 0.05
 imsize_s     = 256
 pixel_s      = 0.8
 
+# tp stuff
+nvgrp        = 4
+
 # pick a few niter values for tclean to check flux convergence 
 niter        = [0,500,1000,2000]
-niter        = [0,100]
-niter        = [0,100,1000]
+#niter        = [0,100]
+#niter        = [0,100,1000]
+niter        = [0,1000]
 
 # pick which ALMA configurations you want (0=7m ACA , 1,2,3...=12m ALMA)
 #cfg          = [0,1,2,3,4,5,6,7,8,9,10]
-cfg          = [0,1]
+cfg          = [0,1,2,3]
 
 # also do a run with a startmodel?
-#startmodel   = 0
+# startmodel   = 0
 
 # single pointing?  Set grid to a positive arcsec grid spacing if the field needs to be covered
-grid         = 25
+#                   ALMA normally uses lambda/2D
+grid         = 30
+#grid         = 12.5    # for the trial 25m dish
+#grid         = 50.0
+#grid         = 0
+
+# dish size in m; uvmax will be taken as 5/6 of this
+dish         = 12.0
+#dish         = 25.0
 
 # -- do not change parameters below this ---
 import sys
@@ -58,48 +70,52 @@ qac_version()
 tp2vis_version()
 
 if grid > 0:
-    # create a mosaic of pointings
-    qac_im_ptg(phasecenter,imsize_m,pixel_m,grid,outfile=ptg)
-    # no support for ptg = 
+    # create a mosaic of pointings for 12m, that's overkill for the 7m
+    p = qac_im_ptg(phasecenter,imsize_m,pixel_m,grid,rect=True,outfile=ptg)
 else:
     # create a single pointing 
     qac_ptg(phasecenter,ptg)
-    # ptg = [phasecenter]
+    p = [phasecenter]
 
 
+qac_log("TP2VIS")
 
-# start with clean new project
-os.system('rm -rf %s' % test)
-
+# create TP MS  - this starts a new clean project
+if True:
+    qac_tpdish('ALMATP',dish)    # e.g. 25m
+    qac_tpdish('VIRTUAL',dish)   # e.g. 12m
+tpms = qac_tp_vis(test,model,ptg,imsize_s,pixel_s,phasecenter=phasecenter,deconv=False,maxuv=5*dish/6.0,nvgrp=nvgrp,fix=0)
+qac_clean1(test + '/clean0', tpms, imsize_s, pixel_s, phasecenter=phasecenter)
 
 # create an MS based on a model and antenna configuration for ACA/ALMA
-qac_log("QAC_ALMA")
+qac_log("ALMA 7m/12m")
 ms1={}
 for c in cfg:
-    ms1[c] = qac_alma(test,model,imsize_m,pixel_m,cycle=5,cfg=c,ptg=ptg, phasecenter=phasecenter,niter=-1)
-# startmodel (a cheat) for later
-startmodel = ms1[0].replace('.ms','.skymodel')
-
-# create TP MS
-tpms = qac_tp_vis(test + '/tp2vis',model,ptg,imsize_s,pixel_s,niter=-1,phasecenter=phasecenter,fix=0)
-
-
+    ms1[c] = qac_alma(test,model,imsize_m,pixel_m,cycle=5,cfg=c,ptg=ptg, phasecenter=phasecenter)
+# startmodel for later
+startmodel = ms1[cfg[0]].replace('.ms','.skymodel')
 
 # find out which MS we got for the INT, or use intms = ms1.values()
-import glob
-intms = glob.glob(test+"/*.ms")
-print "MS files found: ",intms
+intms = ms1.values()
+
+tp2vispl(intms+[tpms],outfig=test+'/tp2vispl.png')
+    
 
 # JD clean for tp2vis
 qac_log("CLEAN with TP2VIS")
-qac_clean(test+'/jd1',tpms,intms,imsize_s,pixel_s,niter=niter,phasecenter=phasecenter,do_concat=False)
+qac_clean(test+'/clean3',tpms,intms,imsize_s,pixel_s,niter=niter,phasecenter=phasecenter,do_int=True,do_concat=False)
 for idx in range(1,len(niter)):
-    inamed = test+'/jd1/tpalma'
-    inamec = test+'/jd1/tpalma_%d' % (idx+1)
+    inamed = test+'/clean3/tpint'
+    inamec = test+'/clean3/tpint_%d' % (idx+1)
     tp2vistweak(inamed,inamec)
+    # really don't need these, check with clean1
+    inamed = test+'/clean3/int'
+    inamec = test+'/clean3/int_%d' % (idx+1)
+    tp2vistweak(inamed,inamec)
+    
 
 qac_log("CLEAN")
-# clean this interferometric map a bit
+# clean just interferometric map a bit
 qac_clean1(test+'/clean1',intms,  imsize_s, pixel_s, phasecenter=phasecenter,niter=niter)
 #qac_clean1(test+'/clean2',intms,  imsize_s, pixel_s, phasecenter=phasecenter,niter=niter,startmodel=startmodel)
 
@@ -122,6 +138,7 @@ qac_stats(model)
 qac_log("REGRESSION")
 
 qac_stats(model)
+qac_stats(test+'/clean3/tpint.image')
 qac_stats(test+'/clean1/dirtymap.image.pbcor')
 qac_stats(test+'/clean1/dirtymap_2.image.pbcor')
 qac_stats(test+'/clean1/dirtymap_3.image.pbcor')
@@ -145,13 +162,15 @@ qac_log("DONE!")
 qac_end()
 
 if True:
-    a1 = 'sky1/clean1/dirtymap.image'
-    a2 = 'sky1/clean1/dirtymap.image.pbcor'
-    a3 = 'sky1/clean1/skymodel.smooth.image'
-    a4 = 'sky1/clean1/feather.image/'
-    a5 = 'sky1/clean1/ssc.image/'
-    a6 = 'sky1/test1-alma.aca.cycle5.skymodel/'
-    qac_plot_grid([a1,a3,a4,a5])
+    a1 = test+'/clean1/dirtymap.image'
+    a2 = test+'/clean1/dirtymap.image.pbcor'
+    a3 = test+'/clean1/skymodel.smooth.image'
+    a4 = test+'/clean1/feather.image/'
+    a5 = test+'/clean1/ssc.image/'
+    a6 = test+'/test1-alma.aca.cycle5.skymodel/'
+    a7 = test+'/clean3/tpint_2.image'
+    a8 = test+'/clean3/tpint_2.tweak.image'
+    qac_plot_grid([a1,a3,a4,a5,a7,a8],plot=test+'/plot1.cmp.png')
 
 
 
