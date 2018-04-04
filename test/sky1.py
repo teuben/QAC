46751c8f1ab3fec025449767c07bca5994779aab
Peter Teuben <teuben@gmail.com>
2018-04-04 14:56:54 -0400

    more generaliztions

--- test/sky1.py
+++ test/sky1.py
@@ -1,6 +1,6 @@
 # -*- python -*-
 #
-#  Play with the skymodel for SD2018
+#  Play with the skymodel 
 #     - one or full pointing set
 #     - options for tp2vis, feather, ssc
 #
@@ -40,6 +40,7 @@ niter        = [0,1000]
 # pick which ALMA configurations you want (0=7m ACA , 1,2,3...=12m ALMA)
 #cfg          = [0,1,2,3,4,5,6,7,8,9,10]
 cfg          = [0,1,2,3]
+times        = [2, 1]     # 2 hrs in 1 min integrations
 
 # also do a run with a startmodel?
 # startmodel   = 0
@@ -55,14 +56,18 @@ grid         = 30
 dish         = 12.0
 #dish         = 25.0
 
+# scaling factors
+wfactor      = 0.1
+afactor      = 1      # not implemented
+
 # -- do not change parameters below this ---
 import sys
 for arg in qac_argv(sys.argv):
     exec(arg)
 
+# derived parameters
 ptg = test + '.ptg'              # pointing mosaic for the ptg
     
-
 # report, add Dtime
 qac_begin(test)
 qac_log("REPORT")
@@ -84,14 +89,16 @@ qac_log("TP2VIS")
 if True:
     qac_tpdish('ALMATP',dish)    # e.g. 25m
     qac_tpdish('VIRTUAL',dish)   # e.g. 12m
-tpms = qac_tp_vis(test,model,ptg,imsize_s,pixel_s,phasecenter=phasecenter,deconv=False,maxuv=5*dish/6.0,nvgrp=nvgrp,fix=0)
-qac_clean1(test + '/clean0', tpms, imsize_s, pixel_s, phasecenter=phasecenter)
+maxuv = 5.0*dish/6.0
+tpms = qac_tp_vis(test,model,ptg,imsize_s,pixel_s,phasecenter=phasecenter,deconv=False,maxuv=maxuv,nvgrp=nvgrp,fix=0)
+tp2viswt(tpms,wfactor,'multiply')
+qac_clean1(test+'/clean0', tpms, imsize_s, pixel_s, phasecenter=phasecenter)
 
 # create an MS based on a model and antenna configuration for ACA/ALMA
 qac_log("ALMA 7m/12m")
 ms1={}
 for c in cfg:
-    ms1[c] = qac_alma(test,model,imsize_m,pixel_m,cycle=5,cfg=c,ptg=ptg, phasecenter=phasecenter)
+    ms1[c] = qac_alma(test,model,imsize_m,pixel_m,cycle=5,cfg=c,ptg=ptg, phasecenter=phasecenter, times=times)
 # startmodel for later
 startmodel = ms1[cfg[0]].replace('.ms','.skymodel')
 
@@ -99,39 +106,44 @@ startmodel = ms1[cfg[0]].replace('.ms','.skymodel')
 intms = ms1.values()
 
 tp2vispl(intms+[tpms],outfig=test+'/tp2vispl.png')
-    
+
+
+# clean just interferometric map a bit (same as do_int=True in clean3,clean4)
+#qac_log("CLEAN")
+#qac_clean1(test+'/clean1',intms,  imsize_s, pixel_s, phasecenter=phasecenter,niter=niter)
+#qac_clean1(test+'/clean2',intms,  imsize_s, pixel_s, phasecenter=phasecenter,niter=niter,startmodel=startmodel)
+
 
 # JD clean for tp2vis
 qac_log("CLEAN with TP2VIS")
 qac_clean(test+'/clean3',tpms,intms,imsize_s,pixel_s,niter=niter,phasecenter=phasecenter,do_int=True,do_concat=False)
-for idx in range(1,len(niter)):
-    inamed = test+'/clean3/tpint'
-    inamec = test+'/clean3/tpint_%d' % (idx+1)
-    tp2vistweak(inamed,inamec)
-    # really don't need these, check with clean1
-    inamed = test+'/clean3/int'
-    inamec = test+'/clean3/int_%d' % (idx+1)
-    tp2vistweak(inamed,inamec)
-    
+qac_tweak(test+'/clean3','int',niter)
+qac_tweak(test+'/clean3','tpint',niter)
 
-qac_log("CLEAN")
-# clean just interferometric map a bit
-qac_clean1(test+'/clean1',intms,  imsize_s, pixel_s, phasecenter=phasecenter,niter=niter)
-#qac_clean1(test+'/clean2',intms,  imsize_s, pixel_s, phasecenter=phasecenter,niter=niter,startmodel=startmodel)
+qac_clean(test+'/clean4',tpms,intms,imsize_s,pixel_s,niter=niter,phasecenter=phasecenter,do_int=True,do_concat=False,startmodel=startmodel)
+qac_tweak(test+'/clean4','int',niter)
+qac_tweak(test+'/clean4','tpint',niter)
+
+
 
 qac_log("OTF")
-# create an OTF TP map using a 12m dish
-qac_tp_otf(test+'/clean1', startmodel, 12.0)
-#qac_tp_otf(test+'/clean2', startmodel, 12.0)
+# create an OTF TP map using a [12m] dish
+#qac_tp_otf(test+'/clean1', startmodel, dish)
+#qac_tp_otf(test+'/clean2', startmodel, dish)
+qac_tp_otf(test+'/clean3', startmodel, dish,template=test+'/clean3/int.image')
+qac_tp_otf(test+'/clean4', startmodel, dish,template=test+'/clean4/int.image')
 
 qac_log("FEATHER")
 # combine TP + INT using feather and ssc, for all niter's
 for idx in range(len(niter)):
-    qac_feather(test+'/clean1', niteridx=idx)
-    qac_ssc(test+'/clean1', niteridx=idx)
-    qac_smooth (test+'/clean1', startmodel, niteridx=idx)
-    #qac_feather(test+'/clean2', niteridx=idx)
-    #qac_smooth (test+'/clean2', startmodel, niteridx=idx)
+    qac_feather(test+'/clean3',             niteridx=idx, name="int")
+    qac_ssc    (test+'/clean3',             niteridx=idx, name="int")
+    qac_smooth (test+'/clean3', startmodel, niteridx=idx, name="int")
+    
+    qac_feather(test+'/clean4',             niteridx=idx, name="int")
+    qac_ssc    (test+'/clean4',             niteridx=idx, name="int")
+    qac_smooth (test+'/clean4', startmodel, niteridx=idx, name="int")
+    
 # the real flux
 qac_stats(model)
 
@@ -161,7 +173,37 @@ qac_stats(test+'/clean1/skymodel.smooth.image')
 qac_log("DONE!")
 qac_end()
 
-if True:
+
+a1 = test+'/clean1/dirtymap.image'       # INT
+a2 = test+'/clean1/dirtymap_2.image'
+
+a3 = test+'/clean2/dirtymap.image'       # INT w/ startmodel
+a4 = test+'/clean2/dirtymap_2.image'
+
+a31 = test+'/clean3/int.image'           # INT/TPINT
+a32 = test+'/clean3/int_2.image'
+a33 = test+'/clean3/tpint.image'
+a34 = test+'/clean3/tpint_2.image'
+
+a41 = test+'/clean4/int.image'           # INT/TPINT w/ startmodel
+a42 = test+'/clean4/int_2.image'
+a43 = test+'/clean4/tpint.image'
+a44 = test+'/clean4/tpint_2.image'
+
+
+qac_tp_otf(test+'/clean1', startmodel, 12.0)
+
+# nitering
+qac_plot_grid([a31,a32,a41,a42,a31,a32,a33,a34],diff=10, plot=test+'/plot1.cmp.png')
+#
+qac_plot_grid([a31,a41,a32,a42],diff=1,  plot=test+'/plot2.cmp.png')
+qac_plot_grid([a33,a43,a34,a44],diff=10, plot=test+'/plot3.cmp.png')
+
+# qac_plot_grid([a1,a31,a2,a32,a3,a41,a4,a42],diff=1)     these are all 0, as they should be
+
+
+
+if False:
     a1 = test+'/clean1/dirtymap.image'
     a2 = test+'/clean1/dirtymap.image.pbcor'
     a3 = test+'/clean1/skymodel.smooth.image'
