afa05317a034e86b8104b1745c6f2707ae0a7f60
jturne19 <jturne19@uwyo.edu>
2018-04-17 12:24:54 -0600

    updated with brian mason fix for SSA primary beam

--- test/test3.py
+++ test/test3.py
@@ -1,6 +1,7 @@
 # test3 - short spacing array for total power
-# looks like the _utm config file works (maybe?) hard to tell but it does not give the "# Note: diameters in configuration file will not be used - PB for NGVLA will be used" error in the log
 #
+# has fix from brian mason memo 43 for getting correct primary beam for the short baseline array (6 m antennae)
+# get terrible flux recovery when feathered together --> SWcore dirtymap_2 gives 120, SSA dirtymap_2 gives 57, feather_2 gives 67, expected 144
 #
 #  it is assumed you have done    execfile('qac.py')
 #
@@ -25,6 +26,8 @@ niter = [0,1000]
 # decide if you want the whole cube (chans=-1) or just a specific channel
 chans        = '-1' # must be a string. for a range of channels --> '24~30'
 
+# if you want multiscale cleaning, then True. if not, then False
+multiscale   = True
 
 # -- do not change parameters below this ---
 import sys
@@ -41,6 +44,11 @@ if chans != '-1':
     # rewrite the model variable with our new model
     model = model_out
 
+if multiscale == True:
+    scales = [0,5,15]
+else:
+    scales = None
+
 ptg = test + '.ptg'              # use a single pointing mosaic for the ptg
 if type(niter) != type([]): niter = [niter]
 
@@ -56,16 +64,27 @@ qac_ptg(phasecenter,ptg)
 # create a MS based on a model and antenna configuration
 qac_log('VLA')
 # first for SSA cfg
+
+# to get create custom voltage pattern table for SSA (from brian mason memo 43)
+# really only have to do this once because the table can then be loaded
+# vp.reset()
+# vp.setpbairy(telescope='NGVLA', dishdiam=6.0, blockagediam=0.0, maxrad='3.5deg', reffreq='1.0GHz', dopb=True)
+# vp.saveastable('sba.tab')
+
+# load vp table
+vp.reset()
+vp.loadfromtable('sba.tab')
 qac_vla(test,model,imsize_m,pixel_m,cfg=0,ptg=ptg, phasecenter=phasecenter)
 # second for SWcore
+vp.reset()
 qac_vla(test,model,imsize_m,pixel_m,cfg=1,ptg=ptg, phasecenter=phasecenter)
 
 # clean this interferometric map a bit
 qac_log('CLEAN')
 # SSA clean
-qac_clean1(test+'/clean_SSA',test+'/'+test+'.'+'ngvlaSA_2b_utm.ms',  imsize_s, pixel_s, phasecenter=phasecenter, niter=niter, restoringbeam='common')
+qac_clean1(test+'/clean_SSA',test+'/'+test+'.'+'ngvlaSA_2b_utm.ms',  imsize_s, pixel_s, phasecenter=phasecenter, niter=niter, restoringbeam='common', scales=scales, vptable='sba.tab')
 # SWCore clean
-qac_clean1(test+'/clean1', test+'/'+test+'.SWcore.ms', imsize_s, pixel_s, phasecenter=phasecenter, niter=niter, restoringbeam='common')
+qac_clean1(test+'/clean1', test+'/'+test+'.SWcore.ms', imsize_s, pixel_s, phasecenter=phasecenter, niter=niter, restoringbeam='common', scales=scales)
 
 # combine SSA + SWCore using feather, for all niters
 qac_log('FEATHER')
