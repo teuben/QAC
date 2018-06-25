4ff452f213885827473dc638ba1f0594728f8e8f
Peter Teuben <teuben@gmail.com>
2018-06-24 23:33:05 -0400

    fixes

--- ngVLA/mapping1.py
+++ ngVLA/mapping1.py
@@ -23,8 +23,9 @@ pixel_m      = 0.005
 # pick the sky imaging parameters (for tclean)
 # The product of these typically will be the same as that of the model (but don't need to)
 # pick the pixel_s based on the largest array configuration (see below) choosen
+# negativ pixel will force the _m and _s size to be the same
 imsize_s     = 512
-pixel_s      = 0.04
+pixel_s      = -1
 
 # pick a few niter values for tclean to check flux convergence
 niter        = [0,1000,2000]
@@ -54,6 +55,9 @@ ptg  = pdir + '.ptg'              # pointing mosaic for the ptg
 
 if niter==0:   niter=[0]          # be nice to allow this, but below it does need to be a list
 
+if pixel_s < 0:                   # force the model and sky size the same
+    pixel_s = imsize_m * pixel_m / imsize_s
+
 
 # report, add Dtime
 qac_begin(pdir,False)
@@ -64,8 +68,9 @@ qac_version()
 p = qac_im_ptg(phasecenter,imsize_m,pixel_m,grid,outfile=ptg)
 print "Using %d pointings for 18m and grid=%g on fieldsize %g arcsec" % (len(p), grid, imsize_m*pixel_m)
 
-# start with a clean project
+# start with a clean project, and use the clean1 directory for all tclean results
 qac_project(pdir)
+cdir = pdir + '/clean1'
 
 # create an MS based on a model and for each ngVLA antenna configuration
 # the emperical procedure using the noise= keyword and using qac_noise() is courtesy Carilli et al. (2017)
@@ -79,38 +84,38 @@ startmodel = ms1.replace('.ms','.skymodel')
 qac_log("CLEAN")
 if grid > 0:
     # the default is mosaicing
-    qac_clean1(pdir+'/clean3', ms1, imsize_s, pixel_s, phasecenter=phasecenter, niter=niter, scales=scales)
+    qac_clean1(cdir, ms1, imsize_s, pixel_s, phasecenter=phasecenter, niter=niter, scales=scales)
 else:
     # but for single pointing we can use the standard gridder
-    qac_clean1(pdir+'/clean3', ms1, imsize_s, pixel_s, phasecenter=phasecenter, niter=niter, scales=scales, gridder='standard')
+    qac_clean1(cdir, ms1, imsize_s, pixel_s, phasecenter=phasecenter, niter=niter, scales=scales, gridder='standard')
 
 qac_log("BEAM")
-qac_beam(pdir+'/clean3/dirtymap.psf', plot=pdir+'/clean3/dirtymap.beam.png')
+qac_beam(cdir+'/dirtymap.psf', plot=cdir+'/dirtymap.beam.png')
 
 qac_log("SMOOTH")
-qac_smooth (pdir+'/clean3', startmodel, name="dirtymap")
+qac_smooth (cdir, startmodel, name="dirtymap")
 
 qac_log("PLOT")
-qac_plot(pdir+'/clean3/skymodel.smooth.image')
-qac_plot(pdir+'/clean3/dirtymap.psf')
-qac_plot(pdir+'/clean3/dirtymap.pb')
+qac_plot(cdir+'/skymodel.smooth.image')
+qac_plot(cdir+'/dirtymap.psf')
+qac_plot(cdir+'/dirtymap.pb')
 
 # check the fluxes
 qac_log("REGRESSION")
 
 for idx in range(len(niter)):
-    qac_stats(pdir+'/clean3/dirtymap%s.image'         %            QAC.label(idx))    
-    qac_stats(pdir+'/clean3/dirtymap%s.image.pbcor'   %            QAC.label(idx))
-    qac_plot (pdir+'/clean3/dirtymap%s.image'         %            QAC.label(idx))        
-    qac_plot (pdir+'/clean3/dirtymap%s.image.pbcor'   %            QAC.label(idx))    
-qac_stats(pdir+'/clean3/skymodel.smooth.image')
+    qac_stats(cdir+'/dirtymap%s.image'         %            QAC.label(idx))    
+    qac_stats(cdir+'/dirtymap%s.image.pbcor'   %            QAC.label(idx))
+    qac_plot (cdir+'/dirtymap%s.image'         %            QAC.label(idx))        
+    qac_plot (cdir+'/dirtymap%s.image.pbcor'   %            QAC.label(idx))    
+qac_stats(cdir+'/skymodel.smooth.image')
 qac_stats(model)
 
 # make a bunch of plots
 idx0 = len(niter)-1    # index of last niter[] for plotting
-a0 = pdir+'/clean3/dirtymap.image'                  
-a1 = pdir+'/clean3/dirtymap%s.image'                  %            QAC.label(idx0)
-a2 = pdir+'/clean3/skymodel.smooth.image'
+a0 = cdir+'/dirtymap.image'                  
+a1 = cdir+'/dirtymap%s.image'                  %            QAC.label(idx0)
+a2 = cdir+'/skymodel.smooth.image'
 
 qac_plot_grid([a0,a1,a0,a2,a1,a2],diff=10, plot=pdir+'/plot1.cmp.png', labels=True)  
 
@@ -121,7 +126,7 @@ except:
     print "qac_psd failed"
 
 qac_log("FIDELITY")
-qac_fidelity(pdir+'/clean3/skymodel.smooth.image',pdir+'/clean3/dirtymap%s.image'% QAC.label(idx0), figure_mode=[1,2,3,4,5])
+qac_fidelity(cdir+'/skymodel.smooth.image',cdir+'/dirtymap%s.image'% QAC.label(idx0), figure_mode=[1,2,3,4,5])
 
 qac_log("DONE!")
 qac_end()
