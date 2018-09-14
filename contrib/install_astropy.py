b2cc35e20eafa857358badc94c0cb2fb9e20e941
Peter Teuben <teuben@gmail.com>
2018-09-14 14:17:43 -0400

    no --user

--- contrib/install_astropy.py
+++ contrib/install_astropy.py
@@ -1,8 +1,10 @@
 #
 # See also: http://docs.astropy.org/en/stable/install.html#installing-astropy-into-casa
 #
+# but withouth the --user, so it installs for all users in the current python that casa used
+#
 from setuptools.command import easy_install
-easy_install.main(['--user', 'pip'])
+easy_install.main(['pip'])
 import pip
 pip.main(['install', 'astropy'])
 
