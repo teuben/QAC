0ad6c981183da4dc1221fe43dfc74be99bc5d94f
Peter Teuben <teuben@gmail.com>
2019-08-04 21:19:58 -0400

    casa6 comment

--- contrib/install_astropy.py
+++ contrib/install_astropy.py
@@ -3,6 +3,8 @@
 #
 # but withouth the --user, so it installs for all users in the current python that casa used
 #
+# Note with casa6 may not be needed anymore.
+#
 from setuptools.command import easy_install
 easy_install.main(['pip'])
 import pip
