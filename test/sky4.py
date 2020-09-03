9cff8229be387b36ca4e0dc7e08b746c1f9c34b6
Peter Teuben <teuben@gmail.com>
2020-09-03 08:23:12 -0400

    add final MAC and SSC

--- test/sky4.py
+++ test/sky4.py
@@ -92,7 +92,6 @@ for arg in qac_argv(sys.argv):
 
 # derived parameters
 ptg   = pdir + '.ptg'              # pointing mosaic for the ptg
-test  = pdir                       # compat
 psd   = []                         # accumulate images for qac_psd()
 if maxuv == None:
     maxuv = 5.0*dish/6.0           # for tp2vis
@@ -143,9 +142,9 @@ ms1={}
 for c in cfg:
     if c==0:
         # 3 times integration time in 7m array
-        ms1[c] = qac_alma(test,model,imsize_m,pixel_m,cycle=7,cfg=c,ptg=ptg, phasecenter=phasecenter, times=[3*times[0],times[1]])
+        ms1[c] = qac_alma(pdir,model,imsize_m,pixel_m,cycle=7,cfg=c,ptg=ptg, phasecenter=phasecenter, times=[3*times[0],times[1]])
     else:
-        ms1[c] = qac_alma(test,model,imsize_m,pixel_m,cycle=7,cfg=c,ptg=ptg, phasecenter=phasecenter, times=times)
+        ms1[c] = qac_alma(pdir,model,imsize_m,pixel_m,cycle=7,cfg=c,ptg=ptg, phasecenter=phasecenter, times=times)
 # startmodel for later
 startmodel = ms1[cfg[0]].replace('.ms','.skymodel')
 psd.append(startmodel)
@@ -153,202 +152,228 @@ psd.append(startmodel)
 # get a list of MS we got for the INT
 intms = ms1.values()
 
+qac_log("OTFMAP")
+
+tp_beam = 56.7                    # @todo is this really good enough
+beam = '%sarcsec' % tp_beam
+# otf = model.replace('.fits','.otf')
+otfmap = '%s/skymodel_orig.otf' % pdir
+print("Creating %s" % otfmap)
+imsmooth(model,'gaussian',beam,beam,pa='0deg',outfile=otfmap+'.tmp',overwrite=True)
+imtrans(otfmap+'.tmp',otfmap,order='0132')    # make sure it's an RDSF cubee
+
 qac_log("TP2VIS")
 if otf == 0:
     tpms = qac_tp_vis(pdir,model,ptg,pixel_m,phasecenter=phasecenter,deconv=False,maxuv=maxuv,nvgrp=nvgrp,fix=0)
 else:
-    tp_beam = 56.7     # @todo
-    beam = '%sarcsec' % tp_beam
-    otf = model.replace('.fits','.otf')
-    print("Creating %s" % otf)
-    imsmooth(model,'gaussian',beam,beam,pa='0deg',outfile=otf,overwrite=True)
-    tpms = qac_tp_vis(test,otf,ptg,pixel_m,phasecenter=phasecenter,deconv=True,maxuv=maxuv,nvgrp=nvgrp,fix=0)
-    psd.append(otf)
+    tpms = qac_tp_vis(pdir,otfmap,ptg,pixel_m,phasecenter=phasecenter,deconv=True,maxuv=maxuv,nvgrp=nvgrp,fix=0)
+    psd.append(otfmap)
 tp2viswt(tpms,wfactor,'multiply')
 
 if True:
     qac_log("CLEAN1:")
-    qac_clean1(test+'/clean0', tpms, imsize_s, pixel_s, phasecenter=phasecenter, **args)
+    qac_clean1(pdir+'/clean0', tpms, imsize_s, pixel_s, phasecenter=phasecenter, **args)
     print("ARGS:",args)
 
 qac_log("PLOT and STATS:")
 for idx in range(1):
-    im1 = test+'/clean0/dirtymap%s.image'       % QAC.label(idx)
-    im2 = test+'/clean0/dirtymap%s.image.pbcor' % QAC.label(idx)
+    im1 = pdir+'/clean0/dirtymap%s.image'       % QAC.label(idx)
+    im2 = pdir+'/clean0/dirtymap%s.image.pbcor' % QAC.label(idx)
     qac_plot(im1,mode=1)      # casa based plot w/ colorbar
     qac_stats(im1)            # noise flat
     qac_stats(im2)            # flux flat
     if idx==0: psd.append(im2)
     
 
-tp2vispl(intms+[tpms],outfig=test+'/tp2vispl.png')
+tp2vispl(intms+[tpms],outfig=pdir+'/tp2vispl.png')
 
 if Qsdint:
     # not working yet
     qac_log("SDINT")
-    sdimage = test + '/clean0/dirtymap.image.pbcor'
-    sdpsf = test + '/clean0/dirtymap.psf'
-    qac_sd_int(test+'/clean5',sdimage,intms,sdpsf, imsize_s,pixel_s,niter=niter,phasecenter=phasecenter, **args)
+    sdimage = pdir + '/clean0/dirtymap.image.pbcor'
+    sdpsf = pdir + '/clean0/dirtymap.psf'
+    qac_sd_int(pdir+'/clean5',sdimage,intms,sdpsf, imsize_s,pixel_s,niter=niter,phasecenter=phasecenter, **args)
 
 qac_log("MAC")
 if Qmac:
-    sdimage = test + '/clean0/dirtymap.image.pbcor'
-    qac_mac(test+'/clean6',sdimage,intms, imsize_s,pixel_s,niter=niter,phasecenter=phasecenter, **args)
+    sdimage = pdir + '/clean0/dirtymap.image.pbcor'     # RDSF cube
+    test = pdir + '/clean6'
+    qac_mac(test,sdimage,intms, imsize_s,pixel_s,niter=niter,phasecenter=phasecenter, **args)
     if True:
-        qac_fits(test+'/clean6/int1.image',           test+'/clean6/sky_int0.image_box1.fits',     box=box, stats=True)
-        qac_fits(test+'/clean6/int1.image.pbcor',     test+'/clean6/sky_int1.image_box1.fits',     box=box, stats=True)
-        qac_fits(test+'/clean6/int1.model',           test+'/clean6/sky_int1.model_box1.fits',     box=box, stats=True)
-        qac_fits(test+'/clean6/int2.model',           test+'/clean6/sky_int2.model_box1.fits',     box=box, stats=True)
-        qac_fits(test+'/clean6/int2.image',           test+'/clean6/sky_int2.image_box1.fits',     box=box, stats=True)
-        qac_fits(test+'/clean6/int2.feather',         test+'/clean6/sky_int2.feather_box1.fits',   box=box, stats=True)
-        qac_fits(test+'/clean6/int2.sm',              test+'/clean6/sky_int2.sm_box1.fits',        box=box, stats=True)
-        qac_fits(test+'/clean6/macint.image.pbcor',   test+'/clean6/sky_int3.image_box1.fits',     box=box, stats=True)
-    
+        qac_fits(sdimage,                      test+'/sky_sdimage_box1.fits',        box=box, stats=True)        
+        qac_fits(test+'/int1.image',           test+'/sky_int0.image_box1.fits',     box=box, stats=True, smooth=2)
+        qac_fits(test+'/int1.image.pbcor',     test+'/sky_int1.image_box1.fits',     box=box, stats=True, smooth=2)
+        qac_fits(test+'/int1.model',           test+'/sky_int1.model_box1.fits',     box=box, stats=True, smooth=2)
+        qac_fits(test+'/int2.model',           test+'/sky_int2.model_box1.fits',     box=box, stats=True, smooth=2)
+        qac_fits(test+'/int2.image',           test+'/sky_int2.image_box1.fits',     box=box, stats=True, smooth=2)
+        qac_fits(test+'/int2.feather',         test+'/sky_int2.feather_box1.fits',   box=box, stats=True, smooth=2)
+        qac_fits(test+'/int2.sm',              test+'/sky_int2.sm_box1.fits',        box=box, stats=True, smooth=2)
+        qac_fits(test+'/macint.image.pbcor',   test+'/sky_int3.image_box1.fits',     box=box, stats=True, smooth=2)
+
+    imregrid(imagename=otfmap,                    # input SD needs to be regridded, kind of silly, qac_mac could do that
+             template=sdimage,
+             output=pdir + '/skymodel.otf',
+             overwrite=True)
+    sdimage = pdir + '/skymodel.otf'
+    # make sure this image is RDSF (stokes before freq)
+
+    test = pdir + '/clean7'
+    qac_mac(test,sdimage,intms, imsize_s,pixel_s,niter=niter,phasecenter=phasecenter, **args)
+    if True:
+        qac_fits(sdimage,                      test+'/sky_sdimage_box1.fits',        box=box, stats=True)
+        qac_fits(test+'/int1.image',           test+'/sky_int0.image_box1.fits',     box=box, stats=True, smooth=2)
+        qac_fits(test+'/int1.image.pbcor',     test+'/sky_int1.image_box1.fits',     box=box, stats=True, smooth=2)
+        qac_fits(test+'/int1.model',           test+'/sky_int1.model_box1.fits',     box=box, stats=True, smooth=2)
+        qac_fits(test+'/int2.model',           test+'/sky_int2.model_box1.fits',     box=box, stats=True, smooth=2)
+        qac_fits(test+'/int2.image',           test+'/sky_int2.image_box1.fits',     box=box, stats=True, smooth=2)
+        qac_fits(test+'/int2.feather',         test+'/sky_int2.feather_box1.fits',   box=box, stats=True, smooth=2)
+        qac_fits(test+'/int2.sm',              test+'/sky_int2.sm_box1.fits',        box=box, stats=True, smooth=2)
+        qac_fits(test+'/macint.image.pbcor',   test+'/sky_int3.image_box1.fits',     box=box, stats=True, smooth=2)
 
 qac_log("CLEAN with TP2VIS")
 if False:
-    qac_clean(test+'/clean3',tpms,intms,imsize_s,pixel_s,niter=niter,phasecenter=phasecenter,do_int=True,do_concat=False, **args)
-    qac_tweak(test+'/clean3','int',  niter)
-    qac_tweak(test+'/clean3','tpint',niter)
+    qac_clean(pdir+'/clean3',tpms,intms,imsize_s,pixel_s,niter=niter,phasecenter=phasecenter,do_int=True,do_concat=False, **args)
+    qac_tweak(pdir+'/clean3','int',  niter)
+    qac_tweak(pdir+'/clean3','tpint',niter)
 else:
-    qac_clean(test+'/clean3',tpms,intms,imsize_s,pixel_s,niter=niter,phasecenter=phasecenter,do_int=True,do_concat=False, **args)
-    qac_tweak(test+'/clean3','tpint',niter)
-psd.append(test+'/clean3/tpint.image.pbcor')
-psd.append(test+'/clean3/skymodel.smooth.image')        
+    qac_clean(pdir+'/clean3',tpms,intms,imsize_s,pixel_s,niter=niter,phasecenter=phasecenter,do_int=True,do_concat=False, **args)
+    qac_tweak(pdir+'/clean3','tpint',niter)
+psd.append(pdir+'/clean3/tpint.image.pbcor')
+psd.append(pdir+'/clean3/skymodel.smooth.image')        
 
 
 if Qhybrid:
-    qac_clean(test+'/clean4',tpms,intms,imsize_s,pixel_s,niter=niter,phasecenter=phasecenter,do_int=True,do_concat=False,startmodel=startmodel)
-    qac_tweak(test+'/clean4','int',niter)
-    qac_tweak(test+'/clean4','tpint',niter)
+    qac_clean(pdir+'/clean4',tpms,intms,imsize_s,pixel_s,niter=niter,phasecenter=phasecenter,do_int=True,do_concat=False,startmodel=startmodel)
+    qac_tweak(pdir+'/clean4','int',niter)
+    qac_tweak(pdir+'/clean4','tpint',niter)
 
 if False:
     # clean instead of tclean()
-    qac_clean(test+'/clean5',tpms,intms,imsize_s,pixel_s,niter=niter,phasecenter=phasecenter,do_int=True,do_concat=False,t=True)
-    qac_tweak(test+'/clean5','int',niter)
-    qac_tweak(test+'/clean5','tpint',niter)
+    qac_clean(pdir+'/clean5',tpms,intms,imsize_s,pixel_s,niter=niter,phasecenter=phasecenter,do_int=True,do_concat=False,t=True)
+    qac_tweak(pdir+'/clean5','int',niter)
+    qac_tweak(pdir+'/clean5','tpint',niter)
 
 qac_log("OTF")
 # create an OTF TP map using a [12m] dish
-qac_tp_otf(test+'/clean3', startmodel, dish, template=test+'/clean3/tpint.image')
+qac_tp_otf(pdir+'/clean3', startmodel, dish, template=pdir+'/clean3/tpint.image')
 if Qhybrid:
-    qac_tp_otf(test+'/clean4', startmodel, dish,template=test+'/clean4/tpint.image')
+    qac_tp_otf(pdir+'/clean4', startmodel, dish,template=pdir+'/clean4/tpint.image')
 
 if Qfeather:
     qac_log("FEATHER")
     # combine TP + INT using feather and ssc, for all niter's
     for idx in range(len(niter)):
-        qac_feather(test+'/clean3',             niteridx=idx, name="int")
-        qac_ssc    (test+'/clean3',             niteridx=idx, name="int")
-        qac_smooth (test+'/clean3', startmodel, niteridx=idx, name="int")
+        qac_feather(pdir+'/clean3',             niteridx=idx, name="int")
+        qac_ssc    (pdir+'/clean3',             niteridx=idx, name="int")
+        qac_smooth (pdir+'/clean3', startmodel, niteridx=idx, name="int")
         if Qhybrid:
-            qac_feather(test+'/clean4',             niteridx=idx, name="int")
-            qac_ssc    (test+'/clean4',             niteridx=idx, name="int")
-            qac_smooth (test+'/clean4', startmodel, niteridx=idx, name="int")
+            qac_feather(pdir+'/clean4',             niteridx=idx, name="int")
+            qac_ssc    (pdir+'/clean4',             niteridx=idx, name="int")
+            qac_smooth (pdir+'/clean4', startmodel, niteridx=idx, name="int")
 
 for idx in range(len(niter)):
-    qac_smooth (test+'/clean3', startmodel, niteridx=idx, name="tpint")
+    qac_smooth (pdir+'/clean3', startmodel, niteridx=idx, name="tpint")
 
 
 qac_log("REGRESSION")
 
 qac_stats(model)
-qac_stats(test+'/clean0/dirtymap.image')
-qac_stats(test+'/clean0/dirtymap.image.pbcor')
-qac_stats(test+'/clean3/int.image')
+qac_stats(pdir+'/clean0/dirtymap.image')
+qac_stats(pdir+'/clean0/dirtymap.image.pbcor')
+qac_stats(pdir+'/clean3/int.image')
 
-qac_stats(test+'/clean3/int.image.pbcor')
-qac_stats(test+'/clean3/int_3.image.pbcor')
+qac_stats(pdir+'/clean3/int.image.pbcor')
+qac_stats(pdir+'/clean3/int_3.image.pbcor')
 
-qac_stats(test+'/clean3/tpint.image')
-qac_stats(test+'/clean3/tpint_2.image')
-qac_stats(test+'/clean3/tpint_3.image')
+qac_stats(pdir+'/clean3/tpint.image')
+qac_stats(pdir+'/clean3/tpint_2.image')
+qac_stats(pdir+'/clean3/tpint_3.image')
 
-qac_stats(test+'/clean3/tpint.image.pbcor')
-qac_stats(test+'/clean3/tpint_2.image.pbcor')
-qac_stats(test+'/clean3/tpint_3.image.pbcor')
+qac_stats(pdir+'/clean3/tpint.image.pbcor')
+qac_stats(pdir+'/clean3/tpint_2.image.pbcor')
+qac_stats(pdir+'/clean3/tpint_3.image.pbcor')
 
-qac_stats(test+'/clean3/tpint_2.tweak.image.pbcor')
-qac_stats(test+'/clean3/tpint_3.tweak.image.pbcor')
+qac_stats(pdir+'/clean3/tpint_2.tweak.image.pbcor')
+qac_stats(pdir+'/clean3/tpint_3.tweak.image.pbcor')
 
-qac_stats(test+'/clean3/feather.image.pbcor')
-qac_stats(test+'/clean3/feather_2.image.pbcor')
-qac_stats(test+'/clean3/feather_3.image.pbcor')
+qac_stats(pdir+'/clean3/feather.image.pbcor')
+qac_stats(pdir+'/clean3/feather_2.image.pbcor')
+qac_stats(pdir+'/clean3/feather_3.image.pbcor')
 
-qac_stats(test+'/clean3/ssc.image')
-qac_stats(test+'/clean3/ssc_2.image')
-qac_stats(test+'/clean3/ssc_3.image')
+qac_stats(pdir+'/clean3/ssc.image')
+qac_stats(pdir+'/clean3/ssc_2.image')
+qac_stats(pdir+'/clean3/ssc_3.image')
 
-qac_stats(test+'/clean3/skymodel.smooth.image')
-qac_stats(test+'/clean3/skymodel_2.smooth.image')
-qac_stats(test+'/clean3/skymodel_3.smooth.image')
+qac_stats(pdir+'/clean3/skymodel.smooth.image')
+qac_stats(pdir+'/clean3/skymodel_2.smooth.image')
+qac_stats(pdir+'/clean3/skymodel_3.smooth.image')
 
-qac_stats(test+'/clean3/otf.image')
-qac_stats(test+'/clean3/otf.image.pbcor')
+qac_stats(pdir+'/clean3/otf.image')
+qac_stats(pdir+'/clean3/otf.image.pbcor')
 
 
 
 if Qfeather and Qmac and Qhybrid:
     qac_log("PLOT_GRID plot2/3")    
-    a1 = test+'/clean1/dirtymap.image'       # INT
-    a2 = test+'/clean1/dirtymap_2.image'
+    a1 = pdir+'/clean1/dirtymap.image'       # INT
+    a2 = pdir+'/clean1/dirtymap_2.image'
 
-    a3 = test+'/clean2/dirtymap.image'       # INT w/ startmodel
-    a4 = test+'/clean2/dirtymap_2.image'
+    a3 = pdir+'/clean2/dirtymap.image'       # INT w/ startmodel
+    a4 = pdir+'/clean2/dirtymap_2.image'
 
-    a31 = test+'/clean3/int.image'           # INT/TPINT
-    a32 = test+'/clean3/int_3.image'
-    a33 = test+'/clean3/tpint.image'
-    a34 = test+'/clean3/tpint_3.image'
-    a35 = test+'/clean3/feather.image'    
-    a36 = test+'/clean3/feather_3.image'
-    a37 = test+'/clean3/ssc.image'    
-    a38 = test+'/clean3/ssc_3.image'
+    a31 = pdir+'/clean3/int.image'           # INT/TPINT
+    a32 = pdir+'/clean3/int_3.image'
+    a33 = pdir+'/clean3/tpint.image'
+    a34 = pdir+'/clean3/tpint_3.image'
+    a35 = pdir+'/clean3/feather.image'    
+    a36 = pdir+'/clean3/feather_3.image'
+    a37 = pdir+'/clean3/ssc.image'    
+    a38 = pdir+'/clean3/ssc_3.image'
 
     a5 = []
-    a5.append(test+'/clean3/int.image')
-    a5.append(test+'/clean3/int_2.image')
-    a5.append(test+'/clean3/int_3.image')
-    a5.append(test+'/clean3/tpint.image')
-    a5.append(test+'/clean3/tpint_2.image')
-    a5.append(test+'/clean3/tpint_3.image')
-    a5.append(test+'/clean3/feather.image' )   
-    a5.append(test+'/clean3/feather_2.image')
-    a5.append(test+'/clean3/feather_3.image')
-    a5.append(test+'/clean3/ssc.image')
-    a5.append(test+'/clean3/ssc_2.image')
-    a5.append(test+'/clean3/ssc_3.image')
+    a5.append(pdir+'/clean3/int.image')
+    a5.append(pdir+'/clean3/int_2.image')
+    a5.append(pdir+'/clean3/int_3.image')
+    a5.append(pdir+'/clean3/tpint.image')
+    a5.append(pdir+'/clean3/tpint_2.image')
+    a5.append(pdir+'/clean3/tpint_3.image')
+    a5.append(pdir+'/clean3/feather.image' )   
+    a5.append(pdir+'/clean3/feather_2.image')
+    a5.append(pdir+'/clean3/feather_3.image')
+    a5.append(pdir+'/clean3/ssc.image')
+    a5.append(pdir+'/clean3/ssc_2.image')
+    a5.append(pdir+'/clean3/ssc_3.image')
 
     a6 = []
-    a6.append(test+'/clean3/int_3.image.pbcor')
-    a6.append(test+'/clean3/skymodel.smooth.image')
-    a6.append(test+'/clean3/tpint_3.image.pbcor')
-    a6.append(test+'/clean3/skymodel.smooth.image')
-    a6.append(test+'/clean3/tpint_3.tweak.image.pbcor')
-    a6.append(test+'/clean3/skymodel.smooth.image')
-    a6.append(test+'/clean3/feather_3.image.pbcor')
-    a6.append(test+'/clean3/skymodel.smooth.image')
-    a6.append(test+'/clean3/ssc_3.image')
-    a6.append(test+'/clean3/skymodel.smooth.image')
+    a6.append(pdir+'/clean3/int_3.image.pbcor')
+    a6.append(pdir+'/clean3/skymodel.smooth.image')
+    a6.append(pdir+'/clean3/tpint_3.image.pbcor')
+    a6.append(pdir+'/clean3/skymodel.smooth.image')
+    a6.append(pdir+'/clean3/tpint_3.tweak.image.pbcor')
+    a6.append(pdir+'/clean3/skymodel.smooth.image')
+    a6.append(pdir+'/clean3/feather_3.image.pbcor')
+    a6.append(pdir+'/clean3/skymodel.smooth.image')
+    a6.append(pdir+'/clean3/ssc_3.image')
+    a6.append(pdir+'/clean3/skymodel.smooth.image')
     
     
-    a41 = test+'/clean4/int.image'           # INT/TPINT w/ startmodel
-    a42 = test+'/clean4/int_3.image'
-    a43 = test+'/clean4/tpint.image'
-    a44 = test+'/clean4/tpint_3.image'
+    a41 = pdir+'/clean4/int.image'           # INT/TPINT w/ startmodel
+    a42 = pdir+'/clean4/int_3.image'
+    a43 = pdir+'/clean4/tpint.image'
+    a44 = pdir+'/clean4/tpint_3.image'
 
 
     #qac_tp_otf(test+'/clean1', startmodel, 12.0)
 
     # nitering
-    qac_plot_grid([a31,a32,a41,a42,a31,a32,a33,a34],diff=10, plot=test+'/plot1.cmp.png')
+    qac_plot_grid([a31,a32,a41,a42,a31,a32,a33,a34],diff=10, plot=pdir+'/plot1.cmp.png')
     #
-    qac_plot_grid([a31,a41,a32,a42],diff=1,  plot=test+'/plot2.cmp.png')
-    qac_plot_grid([a33,a43,a34,a44],diff=10, plot=test+'/plot3.cmp.png')
+    qac_plot_grid([a31,a41,a32,a42],diff=1,  plot=pdir+'/plot2.cmp.png')
+    qac_plot_grid([a33,a43,a34,a44],diff=10, plot=pdir+'/plot3.cmp.png')
     #
-    qac_plot_grid(a5, ncol=3, diff=0, plot=test+'/plot5.cmp.png')
+    qac_plot_grid(a5, ncol=3, diff=0, plot=pdir+'/plot5.cmp.png')
 
-    qac_plot_grid(a6, diff=10, plot=test+'/plot6.cmp.png')
+    qac_plot_grid(a6, diff=10, plot=pdir+'/plot6.cmp.png')
     
     
 
@@ -358,15 +383,15 @@ if Qfeather and Qmac and Qhybrid:
 
 if False:
     qac_log("PLOT_GRID plot1")
-    a1 = test+'/clean1/dirtymap.image'
-    a2 = test+'/clean1/dirtymap.image.pbcor'
-    a3 = test+'/clean1/skymodel.smooth.image'
-    a4 = test+'/clean1/feather.image/'
-    a5 = test+'/clean1/ssc.image/'
-    a6 = test+'/test1-alma.aca.cycle5.skymodel/'
-    a7 = test+'/clean3/tpint_2.image'
-    a8 = test+'/clean3/tpint_2.tweak.image'
-    qac_plot_grid([a1,a3,a4,a5,a7,a8],plot=test+'/plot1.cmp.png')
+    a1 = pdir+'/clean1/dirtymap.image'
+    a2 = pdir+'/clean1/dirtymap.image.pbcor'
+    a3 = pdir+'/clean1/skymodel.smooth.image'
+    a4 = pdir+'/clean1/feather.image/'
+    a5 = pdir+'/clean1/ssc.image/'
+    a6 = pdir+'/test1-alma.aca.cycle5.skymodel/'
+    a7 = pdir+'/clean3/tpint_2.image'
+    a8 = pdir+'/clean3/tpint_2.tweak.image'
+    qac_plot_grid([a1,a3,a4,a5,a7,a8],plot=pdir+'/plot1.cmp.png')
 
 # PSD plot comparison of the images we accumulated in psd[]
 if len(psd) > 0:
@@ -377,12 +402,12 @@ if Qexport:
     os.chdir(pdir)
     qac_project('export')
     qac_fits('clean3/skymodel_3.smooth.image',         'export/sky_model_box1.fits',   box=box, stats=True)
-    qac_fits('clean3/int_3.image',                     'export/sky_int0_box1.fits',    box=box, stats=True)    
     qac_fits('clean3/int_3.image.pbcor',               'export/sky_int_box1.fits',     box=box, stats=True)
     qac_fits('clean3/tpint_3.image.pbcor',             'export/sky_tpint_box1.fits',   box=box, stats=True)
     qac_fits('clean3/tpint_3.tweak.image.pbcor',       'export/sky_tweak_box1.fits',   box=box, stats=True)
     qac_fits('clean3/feather_3.image.pbcor',           'export/sky_feather_box1.fits', box=box, stats=True)
-    qac_fits('clean6/macint.image.pbcor',              'export/sky_mac_box1.fits',     box=box, stats=True)
+    qac_fits('clean3/ssc_3.image',                     'export/sky_ssc_box1.fits',     box=box, stats=True)
+    qac_fits('clean7/macint.image.pbcor',              'export/sky_mac_box1.fits',     box=box, stats=True)
     
 
 
