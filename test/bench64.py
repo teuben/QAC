fd57df7d241d7ed5423f6dc3b06d1ad007eb35fd
Peter Teuben <teuben@gmail.com>
2021-03-13 18:39:39 -0500

    add timings

--- test/bench64.py
+++ test/bench64.py
@@ -14,8 +14,13 @@
 #  Memory needed:      ~3.1GB
 #  QAC_STATS: M100_aver_12.ms 1.1768656369821469 0.64537883779913863 0.0004551410673048648 7.054524494539943 0.0
 #  QAC_STATS: bench64/clean/dirtymap.image
-#  0.0010051750193319534 0.018600483986330393 -0.09945070743560791 0.64335471391677856 788.09618841833628 0.0490453304581 
+#  0.0010051750193319534 0.018600483986330393 -0.09945070743560791 0.64335471391677856 788.09618841833628 0.0490453304581
+#  0.00026622876023311965 0.018670628990395365 -0.10945342481136322 0.5947643518447876 210.79759179746944 0.0110557027891
 #
+#  5.7.0
+#  0.00026142015999614838 0.018795033642804188 -0.11751597374677658 0.60142159461975098 204.95546469157998 0.0110837438597  4
+#  0.00026142015999614849 0.01879503364280442  -0.11751597374677658 0.60142159461975098 204.95546469157998 0.0110837438597 
+
 #  Timing examples:
 #
 #  niter
@@ -25,14 +30,19 @@
 #   1000    1   172.46user 25.15system 3:22.33elapsed  97%CPU
 #   1000    2   237.11user 25.86system 2:50.75elapsed 154%CPU
 #   1000    4   355.97user 27.25system 2:36.79elapsed 244%CPU
-#   1000    8   805.84user 30.75system 3:06.98elapsed 447%CPU
-#
+
+#  now with perchanweightdensity=False
+#           1    133.65user 7.82system 2:22.27elapsed  99%CPU
+#           2    177.27user 7.98system 1:48.02elapsed 171%CPU
+#           4    260.72user 8.15system 1:30.98elapsed 295%CPU
+
 #  10000    1   354.79user 28.39system 6:26.30elapsed  99%CPU
 #  10000    2   440.35user 30.75system 5:42.61elapsed 137%CPU
 #  10000    4   611.71user 32.06system 5:24.33elapsed 198%CPU
-#  10000    8  1197.21user 37.27system 5:53.51elapsed 349%CPU
-# 100000    1   359.09user 28.38system 6:30.39elapsed  99%CPU 
-#
+
+# 100000    1   359.09user 28.38system 6:30.39elapsed  99%CPU
+# 100000    2   442.42user 30.85system 5:44.39elapsed 137%CPU 
+# 100000    4   603.49user 33.69system 5:29.40elapsed 193%CPU
 #------------------------------------------------------------------------------------------------------------------------------
 
 # parameters in this benchmark workflow
@@ -60,6 +70,8 @@ qac_project(test)
 
 #   make sure all the files we need are here
 QAC.assertf(ms12)
+
+# line['perchanweightdensity'] = True
 
 qac_clean1(test+'/clean',ms12,nsize,pixel,niter=niter,phasecenter=phasecenter,**line)
 
